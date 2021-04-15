13679de8c1b5b4213fe406d397a5131462d24647
Mike Iovine <mike.iovine7@gmail.com>
2021-04-15 09:08:26 -0700

    Add MDBOptions

--- db/main.cpp
+++ db/main.cpp
@@ -1,5 +1,6 @@
 #include "file.h"
 #include "env.h"
+#include "options.h"
 #include "log_writer.h"
 
 #include <iostream>
@@ -9,4 +10,6 @@ using namespace mdb;
 int main() {
     auto e = Env::CreateDefault();
     LogWriter(e->MakeWriteOnlyIO("test.txt"));
+
+    auto options = MDBOptions(false);
 }
---
+++ include/options.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "env.h"
+
+#include <memory>
+
+namespace mdb {
+
+struct MDBOptions {
+    MDBOptions(bool write_sync_ = false) : 
+        env{ Env::CreateDefault() }, 
+        write_sync{ write_sync_ } {}
+
+    MDBOptions(
+        std::unique_ptr<Env> env_, 
+        bool write_sync_ = false) : 
+        env{ std::move(env_) },
+        write_sync{ write_sync_ } {}
+
+    std::unique_ptr<Env> env;
+    
+    // If true, force sync with disk (e.g. via a call to fsync())
+    // after every write. 
+    const bool write_sync; 
+};
+
+}
